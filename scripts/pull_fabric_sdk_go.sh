26887c04f890275891ae42b721d5430da93a44d3
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2017-12-13 12:40:05 -0500

    WIP: Mutual TLS cherrypick
    Change-Id: I40b30e02d325b5404d1ed7c5af4e072705a027d9
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- scripts/pull_fabric_sdk_go.sh
+++ scripts/pull_fabric_sdk_go.sh
@@ -40,7 +40,7 @@ mkdir -p $GOPATH/src/github.com/hyperledger/fabric-sdk-go
 cd $GOPATH/src/github.com/hyperledger/
 git clone https://gerrit.hyperledger.org/r/fabric-sdk-go
 cd fabric-sdk-go
-git checkout f853354e1f4d3b4dc17169596425f66ad6f7fcbe
+git checkout 3651028a078a1dfa8028866ea37cd0648e95599d
 
 ##[FAB-6523] Bump Fabric version
 #https://gerrit.hyperledger.org/r/#/c/15993/ - [FAB-6523] Bump Fabric version
@@ -49,6 +49,11 @@ git fetch https://gerrit.hyperledger.org/r/fabric-sdk-go refs/changes/93/15993/1
 ##[FAB-6982] - Support Private Data Collection Config
 #https://gerrit.hyperledger.org/r/#/c/15995/ - [FAB-6982] - Support Private Data Collection Config
 git fetch https://gerrit.hyperledger.org/r/fabric-sdk-go refs/changes/95/15995/1 && git cherry-pick FETCH_HEAD
+
+
+##[FAB-6805] - Mutual TLS
+#https://gerrit.hyperledger.org/r/#/c/16049/
+git fetch https://gerrit.hyperledger.org/r/fabric-sdk-go refs/changes/49/16049/6 && git cherry-pick FETCH_HEAD
 
 
 #export FABRIC_SDK_EXTRA_GO_TAGS=channelevents
